cb04edcc8b0a7ab818b8f0724e0f41f985b8b89c
NAVNIT KUMAR <navnitkumarofficial@gmail.com>
2026-02-01 06:46:14 +0530

    Feat: Vendor Invoice Dashboard

--- src/controllers/invoice.controller.js
+++ src/controllers/invoice.controller.js
@@ -1,13 +1,29 @@
-const Invoice = require('../models/Invoice');
-const { successResponse, errorResponse } = require('../utils/response.util');
+const Vendor = require('../models/Vendor');
 
 const getInvoices = async (req, res, next) => {
     try {
-        const query = { customer: req.user._id };
+        const query = {};
+
+        if (req.user.role === 'vendor') {
+            const vendor = await Vendor.findOne({ user: req.user._id });
+            if (!vendor) {
+                return errorResponse(res, 404, 'Vendor profile not found');
+            }
+            query.vendor = vendor._id;
+        } else if (req.user.role === 'customer') {
+            query.customer = req.user._id;
+        }
+        // Admin can see all, or we can enforce filters if needed
+
         if (req.query.orderId) {
             query.order = req.query.orderId;
         }
-        const invoices = await Invoice.find(query).populate('vendor', 'name email');
+
+        const invoices = await Invoice.find(query)
+            .populate('vendor', 'name email businessName')
+            .populate('customer', 'name email')
+            .sort({ createdAt: -1 });
+
         successResponse(res, 200, 'Invoices list', invoices);
     } catch (error) {
         next(error);
