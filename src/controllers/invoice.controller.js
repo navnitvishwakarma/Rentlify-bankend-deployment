9248e7778b9cc7787baca209803d66bc61e09fb9
NAVNIT KUMAR <navnitkumarofficial@gmail.com>
2026-02-01 07:34:33 +0530

    Debug: Add logs to invoice fetch

--- src/controllers/invoice.controller.js
+++ src/controllers/invoice.controller.js
@@ -3,12 +3,15 @@ const Vendor = require('../models/Vendor');
 const getInvoices = async (req, res, next) => {
     try {
         const query = {};
+        console.log(`[DEBUG] getInvoices called by User: ${req.user._id}, Role: ${req.user.role}`);
 
         if (req.user.role === 'vendor') {
             const vendor = await Vendor.findOne({ user: req.user._id });
             if (!vendor) {
+                console.log(`[DEBUG] Vendor profile not found for user ${req.user._id}`);
                 return errorResponse(res, 404, 'Vendor profile not found');
             }
+            console.log(`[DEBUG] Found Vendor Profile: ${vendor._id}`);
             query.vendor = vendor._id;
         } else if (req.user.role === 'customer') {
             query.customer = req.user._id;
@@ -19,11 +22,13 @@ const getInvoices = async (req, res, next) => {
             query.order = req.query.orderId;
         }
 
+        console.log(`[DEBUG] Executing Invoice Query:`, JSON.stringify(query));
         const invoices = await Invoice.find(query)
             .populate('vendor', 'name email businessName')
             .populate('customer', 'name email')
             .sort({ createdAt: -1 });
 
+        console.log(`[DEBUG] Found ${invoices.length} invoices`);
         successResponse(res, 200, 'Invoices list', invoices);
     } catch (error) {
         next(error);
